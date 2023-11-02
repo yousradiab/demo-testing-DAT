f198be6567b1acd88e19d422a8548b228827aeba
Tomas Pesek <pesekt@gmail.com>
2023-11-02 11:05:22 +0100

    fizzBuzz test

--- lib.test.js
+++ lib.test.js
@@ -1,4 +1,5 @@
 import lib from "./lib";
+import fizzBuzz from "./fizzBuzz";
 
 describe("absolute", () => {
   it("should return a positive number if input is positive", () => {
@@ -70,5 +71,36 @@ describe("registerUser", () => {
     const result = lib.registerUser("mosh");
     expect(result).toMatchObject({ username: "mosh" });
     expect(result.id).toBeGreaterThan(0);
+  });
+});
+
+describe("fizzBuzz", () => {
+  it("should throw an exception if input is not a number", () => {
+    const args = ["a", null, undefined, {}, false];
+    args.forEach((a) => {
+      expect(() => {
+        fizzBuzz(a);
+      }).toThrow();
+    });
+  });
+
+  it("should return FizzBuzz if input is divisible by 3 and 5", () => {
+    const args = [15, 30, 45];
+    args.forEach((a) => expect(fizzBuzz(a)).toBe("FizzBuzz"));
+  });
+
+  it("should return Fizz if input is only divisible by 3", () => {
+    const args = [3, 6, 9, 12];
+    args.forEach((a) => expect(fizzBuzz(a)).toBe("Fizz"));
+  });
+
+  it("should return Buzz if input is only divisible by 5", () => {
+    const args = [5, 10, 20, 25];
+    args.forEach((a) => expect(fizzBuzz(a)).toBe("Buzz"));
+  });
+
+  it("should return input if input is not divisible by 3 or 5", () => {
+    const args = [1, 2, 4, 7, 8, 11, 13, 14];
+    args.forEach((a) => expect(fizzBuzz(a)).toBe(a));
   });
 });
